9d96bd497a3517d1e1e129f6d0ebb0f8705bd946
rizhenkolv <rizhenkolv@gmail.com>
2022-10-12 14:06:34 +0200

    commit changes

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const pageImage = document.querySelector('.page_image')
 const btnConfirm = document.querySelector('.btn.confirm')
 const btnCloseModal = document.querySelector('.btn.cancel')
 const btnOpenModal = document.querySelector('.btn')
+const text = document.querySelector('.text')
+const user = document.querySelector('.input')
+const greeting = document.querySelector('.greeting')
 
 btnOpenModal.addEventListener('click', function () {
   modal.classList.remove('hidden')
+  console.log(user.value)
+  text.innerHTML = `Please confirm account creation for ${user.value}`
 })
+
 btnCloseModal.addEventListener('click', function () {
   modal.classList.add('hidden')
+  user.value = ``
 })
+
 btnConfirm.addEventListener('click', function () {
   console.log('click')
   pageImage.classList.remove('hidden')
   pageMain.classList.add('hidden')
   modal.classList.add('hidden')
+  greeting.append(`Hello ${user.value}!!`)
 })
